5c80ddf15e63c96b297643835f9e9acfe18b72d4
zhaojingyu <1563686808@qq.com>
2021-03-11 14:28:32 +0800

    测试文件

--- GidFlowTest/GidFlowTest/AppDelegate.swift
+++ GidFlowTest/GidFlowTest/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("test")
+        print("test 2也基于IE与i我也我")
         print("finish feature")
         return true
     }
